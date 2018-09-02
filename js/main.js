554cdda6281a646b70c12b24b1c1cf5be2f9a058
Luis Waderdorff <walderdorff@live.de>
2018-09-02 15:24:41 +0200

    Fertig

--- js/main.js
+++ js/main.js
@@ -1,7 +1,47 @@
-// Lightbox
+jQuery(document).ready(function($) {
+		$('a[data-rel^=lightcase]').lightcase();
+	});
 
-// Searchbar
-/*
-for (var i = 0; i < 10; i++) {
-// do something in here
+
+  //Check new input in text form
+
+function searchBox () {
+	// Declare variables
+	var input, filter, slides, slide, picture, i;
+	input = document.getElementById('search');
+	filter = input.value.toUpperCase();
+	slides = document.getElementsByClassName('slides');
+	slide = document.getElementsByClassName('slide');
+
+	// Loop through all slide(s) and hide those who dont match the search query
+	for (i = 0; i < slide.length; i++) {
+		boxText = slide[i].getElementsByTagName("p")[0];
+		if (boxText) {
+			if (boxText.innerHTML.toUpperCase().indexOf(filter) > -1) {
+				slide[i].style.display = "";
+			} else {
+				slide[i].style.display = "none";
+			}
+		}
+	}
 }
+
+
+//If doesnt equal alt or title attribute, hide
+
+//If no text show all, hide none
+
+
+/*
+$('#box').keyup(function(){
+   var valThis = $(this).val().toLowerCase();
+    if(valThis == ""){
+        $('.navList > li').show();
+    } else {
+        $('.navList > li').each(function(){
+            var text = $(this).text().toLowerCase();
+            (text.indexOf(valThis) >= 0) ? $(this).show() : $(this).hide();
+        });
+   };
+});
+*/
